83680349c5516c95b1104a8c1617378a80a06402
Dev-nisa <nisabecodin@outlook.com>
2025-05-18 15:48:44 -0400

    control flow/conditional practice task

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -1,47 +1,27 @@
-// variables
-let userRole = "admin";
-let accessLevel;
-let isLoggedIn = true;
-let userMessage;
-let userType = "subscriber";
-let userCategory;
-let isAuthenticated = true;
-let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
-console.log("Authentication Status: ", authenticationStatus);
-// Access Level conditional
-if (userRole === "admin") {
-    accessLevel = "Full access granted";
-} else if (userRole === "manager") {
-    accessLevel = "Limited access granted";
-} else {
-    accessLevel = "No access granted";
-}
-console.log("Access Level: ", accessLevel);
+// practice variables
+let employee = false;
+let dietaryServices;
+let enrolled = false;
+let dietician;
+let subscriber = false;
 
-// Logged-In conditional
-if (isLoggedIn) {
-    if (userRole === "admin") {
-        userMessage = "Welcome, Admin!";
-    } else {
-        userMessage = "Welcome, User!"
-    }  
-} else {
-    userMessage = "Please log in to access the system.";
+// practice access conditional
+if (employee) {
+    dietaryServices = "have access to Dietary Services, and"; 
+    dietician = "don't have access to the Dietician.";   
 }
-console.log("User Message: ", userMessage);
-
-// User type conditional
-switch (userType) {
-    case "admin":
-        userCategory = "Administrator";
-        break;
-    case "manager":
-        userCategory = "Manager";
-        break;
-    case "subscriber":
-        userCategory = "Subscriber";
-        break;
-    default:
-        userCategory = "Unknown";
+else if (enrolled) {
+    dietaryServices = "have access to Dietary Services, and";
+    dietician = "have access to the Dietician!";
+} 
+else if (subscriber){
+    dietaryServices = "have partical access to Dietary Services, and";
+    dietician = "don't have access to Dietician.";
 }
-console.log("User Category: ", userCategory);
+else {
+    dietaryServices = "No access to Dietary Services, and";
+    dietician = "don't have access to Dietician.";
+    console.log("Please subcribe or enroll for access.");
+};
+// output
+console.log("With this account you",dietaryServices, dietician);
