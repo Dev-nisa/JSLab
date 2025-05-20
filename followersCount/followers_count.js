d672a19ade49d93abd66b34a1f1bdd8ca1e9bd2d
Dev-nisa <nisabecodin@outlook.com>
2025-05-19 23:06:42 -0400

    added reset feature

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -17,3 +17,8 @@ function checkCountValue() {
 function displayCount() {
     document.getElementById('countDisplay').innerText = count;
 };
+function resetCount() {
+    count = 0;
+    displayCount();
+    alert("Follower count has been reset!");
+}
