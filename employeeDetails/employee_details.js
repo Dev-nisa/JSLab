3f340159ee4fa7964fc60c0950d83d4bf65222c2
Dev-nisa <nisabecodin@outlook.com>
2025-05-21 23:08:17 -0400

    feature added - find by specialization button

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,8 +1,8 @@
 // variables
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'JavaScript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Python' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Ruby'},
     //... More employee records can be added here
   ];
 
@@ -17,7 +17,7 @@ const employees = [
 
   function displayHREmployees() {
     const hrEmployees = employees.filter(employee => employee.department === 'HR');
-     const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+     const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - ${employee.salary}</p>`).join('');
      document.getElementById('employeesDetails').innerHTML = hrEmployeesDisplay;
 }
 
@@ -28,5 +28,15 @@ function findEmployeeById(employeeId) {
     }
     else{
       document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+     }
+ }
+
+ function findEmployeeBySpecialization(specialization) {
+    const specializedEmployee = employees.find(employee => employee.specialization === specialization);
+    if (specializedEmployee) {
+    document.getElementById('employeesDetails').innerHTML =`<p>${specializedEmployee.id}: ${specializedEmployee.name}: ${specializedEmployee.name} - ${specializedEmployee.department} - $${specializedEmployee.salary} - ${specializedEmployee.specialization}</p>`;
+    }
+    else{
+      document.getElementById('employeesDetails').innerHTML = 'no employee has been found with JavaScript specialization';
      }
  }
